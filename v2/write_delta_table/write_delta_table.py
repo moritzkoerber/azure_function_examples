ba1dd9f096c950598a2c955013e663309a65fdd6
Moritz Körber <koerber.moritz@gmail.com>
2024-07-09 22:19:11 +0200

    Change pathing

--- v2/write_delta_table/write_delta_table.py
+++ v2/write_delta_table/write_delta_table.py
@@ -11,14 +11,16 @@ write_delta_table_bp = func.Blueprint()
 
 
 @write_delta_table_bp.blob_trigger(
-    arg_name="blob", path="mycontainer/{name}.parquet", connection="STA_CONN_STRING"
+    arg_name="blob",
+    path="mycontainer/delta/{name}.parquet",
+    connection="STA_CONN_STRING",
 )
 def delta_table_func(blob: func.InputStream):
     # type(blob.read()) is bytes
     df = pl.read_parquet(io.BytesIO(blob.read()))
 
     df.write_delta(
-        "abfss://test/delta_table",
+        "abfss://mycontainer/delta_table",
         mode="overwrite",
         storage_options={
             "account_name": os.environ["AZ_STA_NAME"],
