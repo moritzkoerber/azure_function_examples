3179118d0cf06dc6425cd2b9449d34b40756bb73
Moritz Körber <koerber.moritz@gmail.com>
2024-09-16 21:40:26 +0200

    Add V1 AF to stack

--- ops/stack.tf
+++ ops/stack.tf
@@ -128,3 +128,61 @@ resource "azurerm_role_assignment" "function_storage_access" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_linux_function_app.af.identity[0].principal_id
 }
+
+############################################
+# AF V1
+############################################
+resource "azurerm_storage_account" "af-sa-v1" {
+  name                              = "afsav1${var.env}${random_id.unique-id.hex}"
+  resource_group_name               = azurerm_resource_group.af-rg.name
+  location                          = azurerm_resource_group.af-rg.location
+  account_tier                      = "Standard"
+  account_replication_type          = "LRS"
+  min_tls_version                   = "TLS1_2"
+  allow_nested_items_to_be_public   = false
+  https_traffic_only_enabled        = true
+  infrastructure_encryption_enabled = true
+  public_network_access_enabled     = true
+
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+  }
+}
+
+resource "azurerm_linux_function_app" "af-v1" {
+  name                = "af-v1-${var.env}${random_id.unique-id.hex}"
+  resource_group_name = azurerm_resource_group.af-rg.name
+  location            = azurerm_resource_group.af-rg.location
+
+  storage_account_name          = azurerm_storage_account.af-sa-v1.name
+  storage_account_access_key    = azurerm_storage_account.af-sa-v1.primary_access_key
+  service_plan_id               = azurerm_service_plan.af-splan.id
+  functions_extension_version   = var.function_version
+  public_network_access_enabled = false
+  https_only                    = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_stack {
+      python_version = var.python_version
+    }
+    application_insights_key = azurerm_application_insights.af-appi.instrumentation_key
+  }
+}
+
+
+resource "azurerm_role_assignment" "function_storage_access-v1" {
+  scope                = azurerm_storage_account.af-sa-v1.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.af.identity[0].principal_id
+}
