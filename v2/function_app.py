ff73b4289c4e2609dbbc2bf30ce38fedb8570dcc
Moritz Körber <koerber.moritz@gmail.com>
2024-10-14 21:45:57 +0200

    Add asyncio example

--- v2/function_app.py
+++ v2/function_app.py
@@ -1,6 +1,7 @@
 import azure.functions as func
 from copy_blobs.copy_blobs import copy_blobs_bp
 from http_trigger.http_trigger import http_bp
+from http_trigger_async_exec.http_trigger_async_execution import http_async_exec_bp
 from simple_blob_trigger.simple_blob_trigger import simple_blob_trigger_bp
 from simple_blob_trigger_managed_identity.simple_blob_trigger_managed_identity import (
     simple_blob_trigger_mi_bp,
@@ -14,6 +15,7 @@ app = func.FunctionApp()
 
 app.register_functions(copy_blobs_bp)
 app.register_functions(http_bp)
+app.register_functions(http_async_exec_bp)
 app.register_functions(simple_blob_trigger_bp)
 app.register_functions(simple_blob_trigger_mi_bp)
 app.register_functions(write_delta_table_bp)
---
+++ v2/http_trigger_async_exec/http_trigger_async_execution.py
@@ -0,0 +1,28 @@
+import asyncio
+
+import aiohttp
+import azure.functions as func
+
+http_async_exec_bp = func.Blueprint()
+
+
+@http_async_exec_bp.route(route="http_trigger_async_exec")
+def http_trigger_async_execution(req: func.HttpRequest) -> func.HttpResponse:
+    async def query_current_time(session: aiohttp.ClientSession, tz: str):
+        async with session.get(
+            f"http://worldtimeapi.org/api/timezone/{tz}"
+        ) as response:
+            data = await response.json()
+            print(
+                f"In timezone {tz} it is currently {data['datetime'].strftime('%H:%M')}"
+            )
+
+    async def main():
+        timezones = ["Europe/Berlin", "America/New_York", "Asia/Tokyo"]
+        async with aiohttp.ClientSession() as s:
+            async with asyncio.TaskGroup() as tg:
+                for tz in timezones:
+                    tg.create_task(query_current_time(s, tz))
+
+    asyncio.run(main())
+    return func.HttpResponse(status_code=200)
